03b7bb9f60969ac58e748b040dbceaf5b6e4dcba
Marvin <marvinthekidhb@gmail.com>
2026-03-30 18:31:32 +0200

    fix tls

---
+++ setup-tls.sh
@@ -0,0 +1,6 @@
+<!-- sudo su 
+curl https://get.acme.sh | sh -s email=marvinthekidhb@gmail.com
+apt install socat
+sh /root/.acme.sh/acme.sh --issue -d docker-dyma.online -d www.docker-dyma.online --standalone \
+--pre-hook "docker compose -f /home/ubuntu/docker-chapitre12-environnement-symfony/docker-compose.prod.yml stop"
+--post-hook "docker compose -f /home/ubuntu/docker-chapitre12-environnement-symfony/docker-compose.prod.yml start" -->
